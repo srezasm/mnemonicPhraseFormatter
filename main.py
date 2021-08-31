f61b5a4b7f357b1e6755aca49f8a58f8021698c4
srezasm <srezas.2017@gmail.com>
2021-08-31 16:22:59 +0430

    encoding done

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
+from enum import Flag
 import requests as req
 import os.path
+import re
 
-# create nmonic words list file function
-def createNmonicWordsListFile():
-    print('creating english.txt...')
+print('__---### WELCOME ###---__')
+start = input('please enter your encoded nmonic phrase for decoding or press Enter to encode your nmonic: ')
 
-    url = 'https://raw.githubusercontent.com/bitcoin/bips/master/bip-0039/english.txt'
-    response = req.get(url)
+if(start==''):
+    def createNmonicWordsListFile():
+        print('creating english.txt...')
 
-    file = open("./english.txt", "w")
-    file.write(response.text)
+        url = 'https://raw.githubusercontent.com/bitcoin/bips/master/bip-0039/english.txt'
+        res = req.get(url)
 
-if(os.path.isfile('./english.txt')):
-    print('english.text nmonic words list file already exists.')
-    answer = input('do you want to override? y/n ')
-    if(answer == 'y'):
+        file = open('./english.txt', 'w')
+        file.write(res.text)
+    
+    #TODO: check sha256 hash of english.text instead of ask for overriding every time
+    if(os.path.isfile('./english.txt')):
+        print('english.text nmonic words list file already exists.')
+        answer = input('do you want to override? y/n ')
+        if(answer == 'y'):
+            createNmonicWordsListFile()
+    else:
         createNmonicWordsListFile()
 
-createNmonicWordsListFile()
+
+    nmonicPhrase = input('please enter your nmonic phrase: ')
+    nmonicList = nmonicPhrase.split(' ')
+    file = open('./english.txt', 'r').read()
+    newNmonic = []
+
+    for word in nmonicList:
+        similar = '\n'.join(re.findall('^' + word[:1] + '.*', file, flags=re.M))
+
+        w = ''
+        for a in word:
+            w += a
+            fa = re.findall('^'+w+'.*', similar, flags=re.M)
+            if(len(fa) > 1):
+                continue
+            else:
+                newNmonic.append(w)
+                break
+
+    print(''.join(newNmonic))
+else:
+    print('decoding process...')
