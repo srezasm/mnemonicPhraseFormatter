92669f2f7bdd932d65f94e604e3d7e69206ce114
srezasm <srezas.2017@gmail.com>
2021-09-03 10:40:49 +0430

    typoes fixed and some optimization applied

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# Nmonic Phrase Formatter
-For each indivisual nmonic words in the bip39, there is a unique starter substring.   
+# Mnemonic Phrase Formatter
+
+For each individual mnemonic words in the bip39, there is a unique starter substring.
 
 For example:
+
 * abandon => aba
 * arrive => arriv
 * cave => cav
 * era => era
 
-So the base idia is to split the unique part out of each nmonic word and stick them after one another by the default order and remake the original nmonic phrase as the same way later on.
+So the base idea is to split the unique part out of each mnemonic word and stick them after one another by the default order and remake the original mnemonic phrase as the same way later on.
 
-### **Be aware this is just an experimental-fun project and this might be not suggested. So all the responsibility of using it is on the user.** 
+## Be aware this is just an experimental-fun project and this might be not suggested. So all the responsibility of using it is on the user
--- main.py
+++ main.py
@@ -3,69 +3,87 @@ import os.path
 import re
 from hashlib import sha256
 
-
-def createNmonicWordsListFile():
+# request and get english mnemonic words from source
+def create_mnemonic_words_file():
     url = 'https://raw.githubusercontent.com/bitcoin/bips/master/bip-0039/english.txt'
     res = req.get(url)
 
-    file = open('./english.txt', 'w')
-    file.write(res.text)
+    with open('./english.txt', 'w') as file:
+        file.write(res.text)
 
 
-def checkEnglishFile():
+# check mnemonic words file checksum
+def check_english_file_checksum():
     if(os.path.isfile('./english.txt')):
-        file = open('./english.txt', 'r').read()
-        if (sha256(file.encode('utf-8')).hexdigest() != "2f5eed53a4727b4bf8880d8f3f199efc90e58503646d9ff8eff3a2ed3b24dbda"):
-            print('english.text file has currupted!')
-            print('remaking english.txt...')
-            createNmonicWordsListFile()
+        with open('./english.txt','r') as file:
+            file_data = file.read()
+            if (sha256(file_data.encode('utf-8')).hexdigest() != "2f5eed53a4727b4bf8880d8f3f199efc90e58503646d9ff8eff3a2ed3b24dbda"):
+                print('english.text file has corrupted!')
+                print('remaking english.txt...')
+                create_mnemonic_words_file()
 
     else:
-        createNmonicWordsListFile()
+        create_mnemonic_words_file()
 
 
 print('__---### WELCOME ###---__')
 
-checkEnglishFile()
+check_english_file_checksum()
 
 start = input(
-    'please enter your encoded nmonic phrase for decoding or press Enter to encode your nmonic: ')
+    'please enter your encoded mnemonic phrase to decode or press Enter to encode your mnemonic: ')
 
+# open `english.txt` file globally
+with open('./english.txt','r') as file:
+    # read mnemonic words file data
+    data = file.read()
 
-if(start == ''):
-    data = open('./english.txt', 'r').read()
-    nmonicPhrase = input('please enter your nmonic phrase: ')
-    nmonicList = nmonicPhrase.split(' ')
-    newNmonic = []
+    # check if user wants to recover the original mnemonic phrase or generate a new formatted one
+    if(start == ''):            
+        mnemonic_phrase = input('please enter your mnemonic phrase: ')
+        mnemonic_list = mnemonic_phrase.split(' ')
+        new_mnemonic = []
 
-    for word in nmonicList:
-        similar = '\n'.join(re.findall(
-            '^' + word[:1] + '.*', data, flags=re.M))
+        # loop on entered mnemonic phrase words
+        for word in mnemonic_list:
+            # find all similar mnemonics
+            similar = '\n'.join(re.findall(
+                '^' + word[:1] + '.*', data, flags=re.M))
 
-        w = ''
-        for char in word:
-            w += char
-            found = re.findall('^'+w+'.*', similar, flags=re.M)
-            if(len(found) > 1):
+            w = ''
+            # loop on every character in nth `word`
+            for char in word:
+                # add character to `w` variable
+                w += char
+                # find all words that starts with `w`
+                found = re.findall('^'+w+'.*', similar, flags=re.M)
+                # if found words are more than 1, go on, elsewise add the found word to `new_mnemonic` list
+                if(len(found) > 1):
+                    continue
+                else:
+                    new_mnemonic.append(w)
+                    break
+
+        # print the `new_mnemonic`
+        print('   ', ''.join(new_mnemonic))
+    else:
+        word = ''
+        original_mnemonic_words = ['']
+
+        # loop on entered characters
+        for char in start:
+            # add character to `word` variable
+            word += char
+
+            # find all mnemonic words that start with `word`
+            found = re.findall('^' + word + '.*', data, flags=re.M)
+
+            # if found words are more than 1, go on, elsewise add the found word to `original_mnemonic_words` list
+            if (len(found) > 1):
                 continue
             else:
-                newNmonic.append(w)
-                break
+                original_mnemonic_words.append(found[0])
+                word = ''
 
-    print('   ', ''.join(newNmonic))
-else:
-    data = open('./english.txt', 'r').read()
-    word = ''
-    nmonic = ['']
-    for char in start:
-        word += char
-
-        found = re.findall('^' + word + '.*', data, flags=re.M)
-
-        if (len(found) > 1):
-            continue
-        else:
-            nmonic.append(found[0])
-            word = ''
-
-    print('  ', ' '.join(nmonic))
+        # print the `original_mnemonic_words`
+        print('  ', ' '.join(original_mnemonic_words))
